e43c0829f63ffea51ee7ce2adcc9ebf04a763352
pkavya02 <91692986+pkavya02@users.noreply.github.com>
2024-08-11 21:11:15 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull pkavya555/sample-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 pkavya555/sample-python-flask-app:latest
